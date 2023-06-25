d36b0b3631bf2c14573bcdd74fa6af4bcc63f73f
Yulian <khantermail@gmail.com>
2023-06-25 13:52:23 +0300

    adding cli.py functionality

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,10 @@
 #!usr/bin/env python3
+import brain_games.scripts.cli
+
 
 def main():
     print('Welcome to the Brain Games!')
+    brain_games.scripts.cli.welcome_user()
 
 
 if __name__ == '__main__':
